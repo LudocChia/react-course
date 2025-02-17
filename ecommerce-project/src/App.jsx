afb915cd62d1fa3435d9edcf2f714fdf7061cca6
ludoc <ludoc1012@outlook.com>
2025-02-17 23:38:07 +0800

    sync all

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 
 import './App.css'
@@ -5,9 +6,10 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route index element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
   )
 }
 
